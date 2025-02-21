eee44cdd0c4f752ee11586f16f38566cd586a7ef
Lucas Ortiz <lucas.ortiz@nyu.edu>
2025-02-20 23:08:48 -0500

    Initial Commit

---
+++ src/main/java/nyc/pikaboy/doctornowbot/DoctorNowBotApplication.java
@@ -0,0 +1,13 @@
+package nyc.pikaboy.doctornowbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorNowBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorNowBotApplication.class, args);
+    }
+
+}
